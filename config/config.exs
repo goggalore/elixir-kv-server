9170c144c7415509543b8794bb84adabf3658bc9
goggalore <junec177@gmail.com>
2020-07-06 15:40:10 -0700

    modify umbrella config

--- config/config.exs
+++ config/config.exs
@@ -8,11 +8,11 @@
 # configurations or dependencies per app, it is best to
 # move said applications out of the umbrella.
 import Config
+config :kv, :routing_table, [{?a..?z, node()}]
 
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-#
+if Mix.env() == :prod do
+  config :kv, :routing_table, [
+    {?a..?m, :"foo@computer-name"},
+    {?n..?z, :"bar@computer-name"}
+  ]
+end
